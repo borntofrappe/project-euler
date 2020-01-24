bc87ff4003e6938ec98bcc5f61fef4c6bae92e96
borntofrappe <borntofrappe@gmail.com>
2020-01-24 10:52:18 +0100

    add working code

--- 018 - Maximum Path Sum I/script.js
+++ 018 - Maximum Path Sum I/script.js
@@ -1,6 +1,21 @@
 function maximumPathSumI(triangle) {
-  // Good luck!
-  return true;
+  const arrays = triangle.map(row => row.map(cell => cell ? [cell] : 0));
+
+  for(let i = 1; i < arrays.length; i += 1) {
+    for(let j = 0; j < arrays[i].length; j += 1) {
+      const current = arrays[i][j][0];
+        const previous = arrays[i - 1][j - 1];
+        const next = arrays[i - 1][j];
+        arrays[i][j] = [];
+        if(previous) {
+          arrays[i][j] = [...arrays[i][j], ...previous.map(item => item + current)];
+        }
+        if(next) {
+          arrays[i][j] = [...arrays[i][j], ...next.map(item => item + current)];
+        }
+    }
+  }
+  return Math.max(...arrays[arrays.length-1].reduce((acc, curr) => [...acc, ...curr], []));
 }
 
 const testTriangle = [[3, 0, 0, 0],
