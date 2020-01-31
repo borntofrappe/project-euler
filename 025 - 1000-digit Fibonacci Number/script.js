6751d751cf48058ed80a186e91324e0e90edbbcb
borntofrappe <borntofrappe@gmail.com>
2020-01-31 14:04:33 +0100

    add working code

--- 025 - 1000-digit Fibonacci Number/script.js
+++ 025 - 1000-digit Fibonacci Number/script.js
@@ -1,6 +1,10 @@
 function digitFibonacci(n) {
-  // Good luck!
-  return n;
+  const digits = [1, 1];
+  while(digits[digits.length - 1] < 10 ** (n - 1)) {
+    const [a, b] = digits.slice(-2);
+    digits.push(a + b)
+  }
+  return digits.length;
 }
 
-digitFibonacci(20);
+digitFibonacci(3);
