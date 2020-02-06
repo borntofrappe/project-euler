273ec730cbd58639ce2b7076a80de2325665f098
borntofrappe <borntofrappe@gmail.com>
2020-02-06 23:51:35 +0100

    add semi working code

--- 030 - Digit n Powers/failure.js
+++ 030 - Digit n Powers/failure.js
@@ -1,17 +1,35 @@
-function digitnPowers(n) {
-  const lowerThreshold = 10;
-  const upperThreshold = n * 9 ** n;
-  const numbers = [];
-  for(let i = lowerThreshold; i < upperThreshold; i += 1) {
-    const digits = `${i}`
-      .split("")
-      .map(num => parseInt(num, 10))
-      .reduce((acc, curr) => acc + curr ** n, 0);
-    if(i === digits) {
-      numbers.push(i);
+function maxDigits(n) {
+  let digits = 1;
+  while(true) {
+    if(10 ** (digits - 1) > digits * 9 ** n) {
+      break;
     }
+    digits += 1;
   }
-  return numbers.reduce((acc, curr) => acc + curr, 0);
+  return digits;
 }
 
-console.log(digitnPowers(3));
+function digitnPowers(n) {
+  const lowerThreshold = 10;
+  const upperThreshold = 10 ** (maxDigits(n) -1);
+  const powers = Array(10).fill(0).map((value, index) => index ** n);
+  let solution = 0;
+  for(let i = lowerThreshold; i < upperThreshold; i += 1) {
+    let number = i;
+    let sum = 0;
+    while(number > 0) {
+      sum += powers[number % 10];
+      if(sum > i) {
+        break;
+      }
+      number = parseInt(number / 10);
+    }
+    if(sum === i) {
+      solution += sum;
+    }
+  }
+
+  return solution;
+}
+
+digitnPowers(5);
--- 030 - Digit n Powers/script.js
+++ 030 - Digit n Powers/script.js
@@ -1,6 +1,43 @@
+function maxDigits(n) {
+  let digits = 1;
+  while(true) {
+    if(10 ** (digits - 1) > digits * 9 ** n) {
+      break;
+    }
+    digits += 1;
+  }
+  return digits;
+}
+
+function nextDigit(n) {
+
+}
 function digitnPowers(n) {
-  // Good luck!
-  return n;
+  const lowerThreshold = 10;
+  const upperThreshold = 10 ** (maxDigits(n) -1);
+  const powers = Array(10).fill(0).map((value, index) => index ** n);
+  let solution = 0;
+  for(let i = lowerThreshold; i < upperThreshold; i += 1) {
+    let number = i;
+    let sum = 0;
+    const digits = i.toString().split();
+    while(number > 0) {
+      const digit = number % 10;
+      digits.push(digit);
+      sum += powers[digits];
+      if(sum > i) {
+        // update i to go up a digit
+        i = nextDigit(i);
+        break;
+      }
+      number = parseInt(number / 10);
+    }
+    if(sum === i) {
+      solution += sum;
+    }
+  }
+
+  return solution;
 }
 
 digitnPowers(5);
